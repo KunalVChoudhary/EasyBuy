6eceb8df6772f27bb2d67600b50eaed6eb1c8584
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-06-18 10:04:19 +0530

    nothing important

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './components/Signup/Signup'
+import Login from './components/Signup/Login'
+import Navbar from './components/Navbar/Navbar'
+import ProductPage from './components/ProductPage/ProductPage'
+import Footer from './components/Footer/Footer'
+import Try1 from './components/try/try1'
+import Filter from './components/Filter/Filter'
+import HomePage from './components/HomePage/HomePage'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
-    <>
-      <Signup></Signup>
-    </>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/' element={
+        <div>
+          <ToastContainer/>
+          <Navbar/>
+          <HomePage/>
+          <Footer/>
+        </div>} />
+      <Route path='/productpage' element={<Try1/>}/>
+    </Routes>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,14 @@ import { store } from './redux/store/store.js'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
