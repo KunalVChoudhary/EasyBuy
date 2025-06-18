ec9a727a3e5b175ab93e9c1382253c414c8b1af6
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-06-18 18:49:23 +0530

    reducerpath fix

--- frontend/src/redux/apiSlice/apiCartSlice.js
+++ frontend/src/redux/apiSlice/apiCartSlice.js
@@ -1,5 +1,6 @@
 import  {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 export const cartApi= createApi({
+    reducerPath: 'cartApi',
     baseQuery:fetchBaseQuery({baseUrl:`${import.meta.env.VITE_API_URL}`,credentials: "include"}),
     tagTypes:['getCart'],
     endpoints:(builder)=>({
--- frontend/src/redux/apiSlice/apiReviewSlice.js
+++ frontend/src/redux/apiSlice/apiReviewSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const reviewApi= createApi({
+    reducerPath: 'reviewApi',
     baseQuery:fetchBaseQuery({baseUrl:`${import.meta.env.VITE_API_URL}`}),
     tagTypes:['getReviews'],
     endpoints:(builder)=>({
