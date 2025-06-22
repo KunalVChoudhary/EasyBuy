3a3ba3b539c97e4900d9d3351480e02d4f151a89
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-06-22 22:09:21 +0530

    populate get request

--- backend/routes/wishList.js
+++ backend/routes/wishList.js
@@ -1,16 +1,15 @@
 const {Router}=require('express')
 const { WishList } = require('../models/wishList')
-const {Types}=require('mongoose')
 
 const route=Router()
 
 route.get('/wishlist',async(req,res)=>{
     try{
-        const wishList=await WishList.findOne({userId:req.user.id});
+        const wishList=await WishList.findOne({userId:req.user.id}).populate('items');
         return wishList? res.status(200).json(wishList.items) : res.status(200).json([])
     }
     catch(err){
-        return res.status(401).json({message:"Please Login before accessing wishList"})
+        return res.status(401).json({message:"Please Login before accessing WishList"})
     }
 })
 
@@ -22,7 +21,7 @@ route.post('/wishlist',async(req,res)=>{
             {userId:req.user.id},
             {$addToSet:{items:productId}
         },{new:true,upsert:true})
-        return res.status(200).json({message:"Item added to wishList"})
+        return res.status(200).json({message:"Item added to WishList"})
     }
     catch(err){
         return res.status(401).json({message:"Please Login before accessing wishList",error:err.message})
@@ -34,12 +33,12 @@ route.delete('/wishlist',async(req,res)=>{
         const productId=req.body.productId;
         const wishList=await WishList.findOneAndUpdate({userId:req.user.id},{$pull:{items:productId}},{new:true});
         if (!wishList) {
-            throw new Error("Wishlist not found");
+            throw new Error("WishList not found");
         }
         return res.status(200).json(wishList);
     }
     catch(err){
-       return  res.status(401).json({message:"Please Login before accessing wishList",error:err.message})
+       return  res.status(401).json({message:"Please Login before accessing WishList",error:err.message})
     }
 })
 
