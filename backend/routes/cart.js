87763fadeb4f2ee486d13b94339c06d620ec0668
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-06-22 09:47:59 +0530

    len route and fix in delete route and populate the get route

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -6,10 +6,19 @@ const route=Router();
 
 route.get('/cart',async(req,res)=>{
     try{const user=await User.findOne({email:req.user.email})
-    const userCart=await Cart.findOne({userId:user.id})
+    const userCart=await Cart.findOne({userId:user.id}).populate('items.productId');
     return res.status(200).json(userCart.items)}
     catch{
         return res.status(401).json({message:'Please Login before accessing cart'})
+    }
+})
+
+route.get('/cart/len',async(req,res)=>{
+    try{const user=await User.findOne({email:req.user.email})
+    const userCart=await Cart.findOne({userId:user.id})
+    return res.status(200).json(userCart.items.length)}
+    catch{
+        return res.status(401)
     }
 })
 
@@ -52,7 +61,7 @@ route.delete('/cart',async(req,res)=>{
     const userCart= await Cart.findOne({userId:user.id})
     const checkItemInCart= userCart.items.find(item=>item.productId==productId)
     if (checkItemInCart){
-        const updatedItemsList=userCart.items.filter((item)=>{item.productId!=productId})
+        const updatedItemsList=userCart.items.filter((item)=>{return item.productId!=productId})
         userCart.items=[...updatedItemsList]
         await userCart.save()
         return res.status(200).json({message:'item delete successfully'})}
